a6374f23e14ec1d8ae25bfb7580bbf66250226d4
QackTsogtoo <138648477+QackTsogtoo@users.noreply.github.com>
2023-08-07 05:24:39 +0000

    .......

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import './App.css';
+import React from 'react';
 import { Button, Space } from 'antd';
-const App = () => {
-  return (
-    <Space wrap>
+
+const App = () => (
+  <Space wrap>
     <Button type="primary">Primary Button</Button>
     <Button>Default Button</Button>
     <Button type="dashed">Dashed Button</Button>
     <Button type="text">Text Button</Button>
     <Button type="link">Link Button</Button>
   </Space>
-  );
-} 
+);
+
 export default App;
