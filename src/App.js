e77abb14bfdf37d96ed0cb7b1309dfeed19d1fad
QackTsogtoo <138648477+QackTsogtoo@users.noreply.github.com>
2023-08-16 07:23:17 +0000

    KEKW

--- src/App.js
+++ src/App.js
@@ -1,14 +1,41 @@
-import React from 'react';
-import { Button, Space } from 'antd';
+import React,{useState,useEffect} from 'react';
+import './App.css';
+const App=()=>{
+    const [fake,setFake]=useState([]);
 
-const App = () => (
-  <Space wrap>
-    <Button type="primary">Primary Button</Button>
-    <Button>Default Button</Button>
-    <Button type="dashed">Dashed Button</Button>
-    <Button type="text">Text Button</Button>
-    <Button type="link">Link Button</Button>
-  </Space>
-);
-
+    useEffect(()=>{
+        // ON LOAD
+        fakestore();
+    },[])
+    const fakestore=async()=>{
+        
+        console.log(fake);
+      
+        const response=await fetch("https://fakestoreapi.com/products");
+        const jsonData=await response.json();
+        setFake(jsonData);
+    }
+    return(
+        <>
+        <h2>Shop</h2>
+        <div className='container'>
+            {fake.map((values)=>{
+                return(
+                    <>
+                   <div className='box'>
+                <div className='content'>
+                    <h5>{values.title}</h5>
+                    <p>{values.description}</p>
+                </div>
+                <img src={values.image}></img>
+            </div> 
+            </>
+                )
+            })}
+            
+        </div>
+            
+        </>
+    )
+}
 export default App;
